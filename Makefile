4eb3057e821ac344be35eabf6e6801806b953d1a
jim@vheavy.com <jim@vheavy.com>
2025-04-25 00:31:53 -0700

    Tests for beacon sleep

--- Makefile
+++ Makefile
@@ -7,8 +7,8 @@ SKETCH_NAME=$(shell basename `pwd`)
 BOARD_OPTIONS = PartitionScheme=min_spiffs
 
 ifeq ($(BOARD),esp32s3)
-	BOARD_OPTIONS := ${BOARD_OPTIONS}
-	PORT=/dev/ttyUSB0
+	BOARD_OPTIONS := PartitionScheme=default_8MB,FlashSize=8M,CDCOnBoot=cdc
+	PORT=/dev/ttyACM0
 endif
 ifeq ($(BOARD),esp32c3)
 	BOARD_OPTIONS := ${BOARD_OPTIONS},CDCOnBoot=cdc
@@ -29,10 +29,8 @@ ${BOARD}.mk:
 		-b esp32:esp32:${BOARD} --board-options ${BOARD_OPTIONS} \
 		-u -p ${PORT} | tee cli.out | bin/cli-parser.py > ${BOARD}.mk
 
-fixtty:
-	stty -F ${PORT} -hupcl -crtscts -echo raw 115200
-cat:    fixtty
-	cat ${PORT} | tee ./cat.out
+cat:    
+	while sleep .01; do if [ -c ${PORT} ]; then stty -F ${PORT} -echo raw 115200 && cat ${PORT}; fi; done  | tee ./cat.`basename ${PORT}`.out
 socat:  
 	socat udp-recvfrom:9000,fork - 
 mocat:
@@ -52,9 +50,11 @@ clean-all:
 # CSIM rules 
 
 CSIM_BUILD_DIR=./build/csim
-CSIM_LIBS=esp32jimlib Arduino_CRC32 ArduinoJson
+CSIM_LIBS=esp32jimlib Arduino_CRC32 ArduinoJson Adafruit_HX711
 CSIM_LIBS+=esp32csim
-CSIM_SRC_DIRS=$(foreach L,$(CSIM_LIBS),${HOME}/Arduino/libraries/${L}/src)
+CSIM_SRC_DIRS+=$(foreach L,$(CSIM_LIBS),${HOME}/Arduino/libraries/${L})
+CSIM_SRC_DIRS+=$(foreach L,$(CSIM_LIBS),${HOME}/Arduino/libraries/${L}/src/csim_include)
+CSIM_SRC_DIRS+=$(foreach L,$(CSIM_LIBS),${HOME}/Arduino/libraries/${L}/src)
 CSIM_SRCS=$(foreach DIR,$(CSIM_SRC_DIRS),$(wildcard $(DIR)/*.cpp)) 
 CSIM_SRC_WITHOUT_PATH = $(notdir $(CSIM_SRCS))
 CSIM_OBJS=$(CSIM_SRC_WITHOUT_PATH:%.cpp=${CSIM_BUILD_DIR}/%.o)
--- sleepTest.ino
+++ sleepTest.ino
@@ -1,5 +1,7 @@
 #include "jimlib.h"
 #include "serialLog.h"
+#include "espNowMux.h"
+#include "reliableStream.h"
 #ifndef CSIM
 #include "rom/uart.h"
 #endif
@@ -7,26 +9,66 @@
 JStuff j;
 CLI_VARIABLE_FLOAT(x, 1);
 
+class BSleepNode {
+    std::map<string,int> macsSeen;
+public: 
+    string mac = getMacAddress().c_str();
+    ReliableStreamESPNow espNow = ReliableStreamESPNow("BSLEEP", true);
+    void run() { 
+        string in = espNow.read();
+        if (in != "") { 
+            OUT("<<<< %s", in.c_str());
+            macsSeen[in]++;
+        }
+        if (j.secTick(1)) { 
+            OUT(">>>> %s", mac.c_str());
+            espNow.write(mac);
+        }
+        if (millis() > 5000) { 
+            float sleepSec = ESPNowMux::Instance->bwakeup.getSleepSec();
+            OUT("macs seen %d sleep %.2f", macsSeen.size(), sleepSec);
+            for(auto m : macsSeen) {
+                OUT("%12s %d", m.first.c_str(), m.second);
+            }
+            if (sleepSec < 0) 
+                sleepSec = 60;
+            deepSleep(1000 * sleepSec);
+        }
+    }
+};
+
 void setup() {
     j.begin();
-    j.cli.on("RESET", [](){ ESP.restart(); });
+    j.jw.enabled = false;
+    //j.cli.on("RESET", [](){ ESP.restart(); });
 }
 
+int seedS3LED = 21;
+BSleepNode client;
 void loop() {
     j.run();
-    OUT("loop %f", (float)x);
-    delay(1000);
+    client.run();
+    delay(1);
 }
 
 #ifdef CSIM
 class SketchCsim : public Csim_Module {
+    BSleepNode client1;
+
     public:
-    void setup() { HTTPClient::csim_onPOST("http://.*/log", 
-        [](const char *url, const char *hdr, const char *data, string &result) {
- 	return 200; }); }
+    void setup() { 
+        HTTPClient::csim_onPOST("http://.*/log", 
+            [](const char *url, const char *hdr, const char *data, string &result) {
+ 	            return 200; });
+        client.mac = "FOOMAC"; 
+        ESPNOW_sendHandler = new ESPNOW_csimOneProg();
+        csim_flags.OneProg = true;
+     }
     string dummy;
     void parseArg(char **&a, char **la) override { if (strcmp(*a, "--dummy") == 0) dummy = *(++a); }
-    void loop() override {}
+    void loop() override {
+        client1.run();
+    }
 } sketchCsim;
 #endif
  
